f59d38fe69e87b8940b664b460dcf32c462f4593
Robert Dailey <git@rdailey.me>
2025-06-04 22:04:21 -0500

    setup delta as pager

--- home/dot_config/bash/platforms/windows.sh
+++ home/dot_config/bash/platforms/windows.sh
@@ -1,5 +1,8 @@
 # Windows-specific configurations
 
+# Prepend Chocolatey bin to PATH to prioritize it over Git for Windows tools
+export PATH="$HOME/AppData/Local/UniGetUI/Chocolatey/bin:$PATH"
+
 # Node.js platform check bypass for Windows environments
 export NODE_SKIP_PLATFORM_CHECK=1
 
