133b626ee6108a9fae8ab908f9c5cdbe9d3bc87f
Robert Dailey <git@rdailey.me>
2025-07-16 20:34:46 -0500

    add gobin to path

--- home/dot_config/bash/path.sh
+++ home/dot_config/bash/path.sh
@@ -9,5 +9,10 @@ if [ -d "$HOME/.local/bin" ] ; then
     PATH="$HOME/.local/bin:$PATH"
 fi
 
+# Go tools PATH - add if go is installed
+if command -v go >/dev/null 2>&1; then
+    export PATH="$(go env GOPATH)/bin:$PATH"
+fi
+
 # Append home dir to path so we can access scripts in there
 export PATH=$PATH:~:~/git-scripts
