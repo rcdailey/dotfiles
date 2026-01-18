c51480b78998fed75795542bc87f8fdc45b61db3
Robert Dailey <git@rdailey.me>
2026-01-18 14:01:09 -0600

    chore(commitlint): ignore WIP commits on topic branches

--- home/dot_config/commitlint/commitlint.config.mjs
+++ home/dot_config/commitlint/commitlint.config.mjs
@@ -1,5 +1,6 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  ignores: [(commit) => /^wip/i.test(commit)],
   rules: {
     // Enforce 72 character line length for body
     'body-max-line-length': [2, 'always', 72],
